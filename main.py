88b996f2e97d56247ccc0429f03291c6c2614cc0
natthasath <natthasath.sak@gmail.com>
2023-04-27 13:33:18 +0700

    Add certs folder and add poetry

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from decouple import config
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host=str(config("APP_HOST")), port=int(config("APP_PORT")), reload=True)
+    # uvicorn.run("app.api:app", host=str(config("APP_HOST")), port=int(config("APP_PORT")), reload=True, ssl_certfile=config("SSL_CERTIFICATE"), ssl_keyfile=config("SSL_KEYFILE"))
